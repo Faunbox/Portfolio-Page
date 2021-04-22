31a1ae396eb78f39c5da6238e7e3fd9df4b7213c
Filip <faunbox2@gmail.com>
2021-04-22 19:48:27 +0200

    add GlobalStyles

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useContext } from "react";
 
-import { AppContext } from './AppContext';
+import { AppContext } from "./AppContext";
 
 import styled from "styled-components";
+import { ThemedWrapperStyle } from "../GlobalCss/GlobalStyles";
 
+//Material UI Components
+import Switch from "@material-ui/core/Switch";
+import { FormControlLabel } from "@material-ui/core";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 import gsap from "gsap/";
@@ -12,36 +16,48 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
+///Wrapper
 const MainSection = styled.section`
-  ///Wrapper
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  // color: white,
-  background-color: ${({theme}) => theme === true ? "black" : "white"};
+  ${ThemedWrapperStyle}
   text-align: center;
-  color: ${({theme}) => theme === true ? "white" : "black"};
-  min-height: 100vh;
   transition: 1s ease;
 `;
 
-const Hello = styled.h1`
+const H1 = styled.h1`
   font-size: 60px;
   font-weight: bold;
 `;
 
-const Intro = styled.h2`
+const H2 = styled.h2`
   font-size: 35px;
   font-weight: normal;
 `;
-const ArrowIcon = styled.div`
+const ArrowIconDiv = styled.div`
   position: fixed;
-  // color: white;
   top: 90%;
   left: 50%;
   z-index: 1;
+  font-size: 3.5rem;
   transform: translate(-50%, -50%);
+`;
+
+const ThemeControl = styled(FormControlLabel)`
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  z-index: 10;
+`;
+
+const ThemeSwitch = styled(Switch)`
+  .Mui-checked {
+    color: white;
+  }
+  .MuiSwitch-track {
+    background-color: white;
+  }
+  .MuiSwitch-colorSecondary.Mui-checked {
+    background-color: black;
+  }
 `;
 
 const Main = () => {
@@ -49,13 +65,13 @@ const Main = () => {
   const mainSectionRef = useRef(null);
   const arrowRef = useRef(null);
 
-  //States
-  const { isDarkTheme } = useContext(AppContext)
+  //Context
+  const { isDarkTheme, toggleIsDarkTheme } = useContext(AppContext);
 
   //Arrow behavior
-  const scrollDown = () => {
+  const scrollDownArrow = () => {
     const arrow = arrowRef.current;
-    gsap.to(window, { duration: 1.3, scrollTo: window.screen.height });
+    gsap.to(window, 1.3, { scrollTo: window.screen.height });
     gsap.to(arrow, 1, { autoAlpha: 0, ease: "easeIn" });
   };
   useEffect(() => {
@@ -63,10 +79,10 @@ const Main = () => {
     const mainSection = mainSectionRef.current;
 
     //Arrow Icon Bouncing
-    const arrowTl= gsap.timeline({ repeat: -1, yoyo: true });
+    const arrowTl = gsap.timeline({ repeat: -1, yoyo: true });
     arrowTl.fromTo(arrow, 1, { y: "+=30", repeat: -1 }, { y: 10 });
 
-    //Arrow Div functionality
+    //Arrow Div opacity
     gsap.fromTo(
       arrow,
       1,
@@ -77,7 +93,6 @@ const Main = () => {
         scrollTrigger: {
           trigger: mainSection,
           start: "10% top",
-          // markers: true,
           toggleActions: "play none none reverse",
         },
       }
@@ -87,6 +102,7 @@ const Main = () => {
   // Animate MainSection on page loads
   useEffect(() => {
     const mainSectionChilldrens = mainSectionRef.current.children;
+
     const tl = gsap.timeline({ duration: 1, ease: "ease-In" });
     [...mainSectionChilldrens].forEach((element) => {
       tl.fromTo(
@@ -100,13 +116,16 @@ const Main = () => {
 
   return (
     <MainSection ref={mainSectionRef} theme={isDarkTheme} id="main">
-      <Hello>Hello!</Hello>
-      <Intro>
-        I'm Filip, <span>Front-End Developer</span>
-      </Intro>
-      <ArrowIcon onClick={scrollDown}>
-        <ArrowDownwardIcon ref={arrowRef} fontSize="large" />
-      </ArrowIcon>
+      <ThemeControl
+        control={<ThemeSwitch onClick={toggleIsDarkTheme} />}
+        label={isDarkTheme ? "White?" : "Dark?"}
+        labelPlacement={"top"}
+      />
+      <H1>Hello!</H1>
+      <H2>I'm Filip, Front-End Developer</H2>
+      <ArrowIconDiv onClick={scrollDownArrow}>
+        <ArrowDownwardIcon ref={arrowRef} fontSize="inherit" />
+      </ArrowIconDiv>
     </MainSection>
   );
 };
